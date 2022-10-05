095ccd0009bd4a57cf6f4b142c848041a8ae0714
christine <chrisitne.brown@bhcc.edu>
2022-10-04 21:38:10 -0400

    equals method

---
+++ src/main/java/Equals.java
@@ -0,0 +1,11 @@
+//public class Equals extends Student{
+//    private int studentId ;
+//    @Override
+//    public boolean equals(Object obj){
+//       Student s = (Student) obj;
+//       if(studentId != s.obj){
+//         return false;
+//       }
+//       return true;
+//    }
+//}
